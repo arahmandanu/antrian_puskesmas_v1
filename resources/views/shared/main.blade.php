520e76be55491988fb6f3d71347f35981497e596
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-08-15 01:13:54 +0700

    add logo

--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -27,9 +27,10 @@
 
     <!-- Header -->
     <header class="bg-green-600 text-white py-4 flex flex-col items-center shadow-lg">
-        <img src="https://via.placeholder.com/100x100?text=Logo" alt="Logo Puskesmas"
+        <img src="{{ asset('images/logo.png') }}" alt="Logo Puskesmas"
             class="w-24 h-24 rounded-full border-4 border-white shadow-lg mb-2">
         <h1 class="text-2xl font-bold tracking-wide">PUSKESMAS SEHAT BERSAMA</h1>
+        <h1 class="text-1xl font-bold tracking-wide">Beluk, Bayat, Klaten</h1>
     </header>
 
     <!-- Main Content -->
