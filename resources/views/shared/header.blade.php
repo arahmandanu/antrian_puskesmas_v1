ea2bf4a9abbc2fa98ffd2b24883b2dedd5b33ab9
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-08-15 01:44:48 +0700

    split header and footer to different file

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,7 @@
+<!-- Header -->
+<header class="bg-green-600 text-white py-4 flex flex-col items-center shadow-lg">
+    <img src="{{ asset('images/logo.png') }}" alt="Logo Puskesmas"
+        class="w-24 h-24 rounded-full border-4 border-white shadow-lg mb-2">
+    <h1 class="text-2xl font-bold tracking-wide">PUSKESMAS SEHAT BERSAMA</h1>
+    <h1 class="text-1xl font-bold tracking-wide">Beluk, Bayat, Klaten</h1>
+</header>
--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -25,20 +25,12 @@
 
 <body class="bg-gray-100 h-screen flex flex-col">
 
-    <!-- Header -->
-    <header class="bg-green-600 text-white py-4 flex flex-col items-center shadow-lg">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo Puskesmas"
-            class="w-24 h-24 rounded-full border-4 border-white shadow-lg mb-2">
-        <h1 class="text-2xl font-bold tracking-wide">PUSKESMAS SEHAT BERSAMA</h1>
-        <h1 class="text-1xl font-bold tracking-wide">Beluk, Bayat, Klaten</h1>
-    </header>
+    @include('shared.header')
 
     <!-- Main Content -->
     @yield('content')
 
-    <footer class="bg-white border-t border-gray-200 py-4 text-center text-sm text-gray-500">
-        &copy; 2025 PT Karya Wiguna. All rights reserved.
-    </footer>
+    @include('shared.footer')
 </body>
 
 </html>
