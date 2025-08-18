6631747ae7655315ccfeb8543a29fec4034bb563
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-08-19 01:21:36 +0700

    remove unused

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -79,11 +78,6 @@ Route::get('/poli_call', function () {
     return view('loket_staff.call');
 });
 
-
 Route::get('/poli_terpanggil', function () {
     return view('pasien.poli_terpanggil');
-});
-
-Route::get('/admin', function () {
-    return view('admin.index');
 });
