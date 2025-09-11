e49942a2039b4aa44541511140417071e79b4a2a
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-09-11 17:37:19 +0700

    te

--- tv-l2.bat
+++ tv-l2.bat
@@ -1,4 +1,4 @@
-start msedge "http://localhost/antrian_puskesmas/play_suara/choosed_lantai_v2/2" --kiosk --user-data-dir=%TEMP%/monitor1 --autoplay-policy=no-user-gesture-required
+start msedge "http://localhost/antrian_puskesmas/play_suara/choosed_lantai/2" --kiosk --user-data-dir=%TEMP%/monitor1 --autoplay-policy=no-user-gesture-required
 
 @REM start firefox --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,1920 -new-tab localhost/antrian_puskesmas
 @REM start firefox --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 -new-tab localhost/antrian_puskesmas/play_suara/choosed_lantai/1
