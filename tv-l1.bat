b5c51803b5f18ac507e7cf991d03497eb1f542c1
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-09-11 09:39:40 +0700

    fix tv

--- tv-l1.bat
+++ tv-l1.bat
@@ -1,4 +1,4 @@
-start firefox --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=1920,0 -new-tab localhost/antrian_puskesmas_v1/play_suara/choosed_lantai_v2/1
+start firefox --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=1920,0 -new-tab localhost/antrian_puskesmas/play_suara/choosed_lantai/1
 
 @REM start firefox --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,1920 -new-tab localhost/antrian_puskesmas
 @REM start firefox --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 -new-tab localhost/antrian_puskesmas/play_suara/choosed_lantai/1
